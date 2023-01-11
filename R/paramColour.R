2baa743f4001f3c63f56e1764dc6141f6c516199
Steve Markham <Nattereri@users.noreply.github.com>
2023-01-11 12:08:26 +0000

    Function paramColour added.

---
+++ R/paramColour.R
@@ -0,0 +1,31 @@
+#' Returns colour of Parameter to show on the graph.
+#'
+#' @param lookUpParameter parameter for which colour is required
+#'
+#' @return Colour as a string
+#'
+#' @examples
+#' paramColour("Conductivity")
+#'
+#' @export
+paramColour <- function (lookUpParameter) {
+
+  unitsList <- c(#Look up table as named vector
+    "Temperature" = "steelblue",
+    "Molybdate"  = "lightseagreen",
+    "Glycol" = "slategrey",
+    "pH" = "darkorange",
+    "Pseudomonas Species" = "yellow3",
+    "Total Viable Count" = "darkgoldenrod3",
+    "Conductivity" = "purple4",
+    "Chloride" = "burlywood",
+    "Nitrate" = "coral3",
+    "Total Iron" =  "darkred",
+    "Total Copper" = "seagreen",
+    "Total Sodium"  = "honeydew4",
+    "TDS"  = "mistyrose2",
+    "Sulphate" = "dodgerblue")
+
+  unitsList[[lookUpParameter]]
+
+}
