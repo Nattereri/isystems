193d04e657e7dd270b01ae0979a5c8b3981d5ca6
Steve Markham <Nattereri@users.noreply.github.com>
2023-01-12 12:37:54 +0000

    Check Excel date is numeric.

--- R/dateExcel.R
+++ R/dateExcel.R
@@ -13,6 +13,9 @@
 #' @export
 dateExcel <- function (excelDate) {
 
+  # check data is data.frame
+  stopifnot("Excel date must be numeric" =  is.numeric(excelDate))
+
   as.Date(excelDate, origin = "1899-12-30")
 
 }
