af8bf7456278da1c6b4fe73ec40991fe0a36f1b6
Steve Markham <Nattereri@users.noreply.github.com>
2023-04-05 10:12:38 +0100

    Water heating function added.

---
+++ R/waterHeatingkWh.R
@@ -0,0 +1,25 @@
+#' Returns kWh needed to change the temperature of water
+#'
+#' @param m - the mass of water - tonnes
+#' @param T_f - the final temperature
+#' @param T_i - the initial temperature
+#' @param Eff - Efficiency of the boiler
+#'
+#'
+#' @return kWh for heater water from an initial to final temperature
+#'
+#' @examples
+#' waterHeatingkWh(68, 15, 60, eff = 0.88)
+#' waterHeatingkWh(3536, 55, 56, eff = 0.88)
+#'
+#' @export
+waterHeatingkWh <- function (m, T_i, T_f, eff = 0.88) {
+
+  c <- 4184
+  JkWh <- 2.7777778 * 10^(-7)
+
+  Q_t = (c * m * (T_f - T_i) * 1000) / eff
+
+  JkWh * Q_t
+
+}
