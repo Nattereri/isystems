1aff48bc12f861bcc7b20e814afaa0612b745e12
Steve Markham <Nattereri@users.noreply.github.com>
2023-04-25 11:05:54 +0100

    factorYrMn amended to include full month names.

--- R/factorYrMn.R
+++ R/factorYrMn.R
@@ -2,16 +2,18 @@
 #'
 #' @param fromYear integer Year
 #' @param toYear integer Year
-#' @param yr_type 2 or 4 digit years
+#' @param abbr_yr logical TRUE 2 digit year FALSE 4 digit year
+#' @param abbr_mn logical TRUE abbreviated month FALSE full month
 #'
 #' @return character vector
 #'
 #' @examples
 #' factorYrMn(2000, 2023)
+#' factorYrMn(2020, 2023, abbr_yr = F, abbr_mn = F)
 #'
 #'
 #' @export
-factorYrMn <- function(fromYear, toYear, yr_type = 2) {
+factorYrMn <- function(fromYear, toYear, abbr_yr = T, abbr_mn = T) {
 
   fromYear <- as.integer(fromYear)
   toYear <- as.integer(toYear)
@@ -21,6 +23,8 @@ factorYrMn <- function(fromYear, toYear, yr_type = 2) {
 
   #Levels
   months <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+
+  monthsfull <- c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
 
   years <- seq(fromYear, toYear, by = 1L)
 
@@ -33,17 +37,22 @@ factorYrMn <- function(fromYear, toYear, yr_type = 2) {
 
       k <- k + 1
 
-      if (yr_type == 4) {
-        month_year[k] <- glue("{months[j]} {years[i]}")
-      } else if (yr_type == 2) {
-
-        year2 <- stringr::str_sub(as.character(years[i]), start = 3L, end = 4L)
-
-        month_year[k] <- glue("{months[j]} {year2}")
+      if (abbr_mn) {
+        if (abbr_yr) {
+          year2 <- stringr::str_sub(as.character(years[i]), start = 3L, end = 4L)
+          month_year[k] <- glue("{months[j]} {year2}")
+        } else {
+          month_year[k] <- glue("{months[j]} {years[i]}")
+        }
+      } else {
+        if (abbr_yr) {
+          year2 <- stringr::str_sub(as.character(years[i]), start = 3L, end = 4L)
+          month_year[k] <- glue("{monthsfull[j]} {year2}")
+        } else {
+          month_year[k] <- glue("{monthsfull[j]} {years[i]}")
+        }
       }
-
     }
-
   }
 
   return(month_year)
