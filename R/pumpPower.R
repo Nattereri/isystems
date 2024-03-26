925070c25a8d4e88c4284e789998335d53ec8ac2
Steve Markham <Nattereri@users.noreply.github.com>
2024-03-26 16:37:03 +0000

    Checked pump calcs

--- R/pumpPower.R
+++ R/pumpPower.R
@@ -1,13 +1,13 @@
 #' Returns pump power (kW) for a head and discharge
 #'
 #' @param Hm pumping head (metres)
-#' @param Q discharge volume (l/s)
+#' @param Q discharge volume (m3/s)
 #' @param eff pump efficency (percentage)
 #'
 #' @return numeric (kW)
 #'
 #' @examples
-#' pumpPower(35, 0.06, eff= 0.55)
+#' pumpPower(1000, 0.0087, eff= 0.7)
 #'
 #' @export
 pumpPower <- function (Hm, Q, eff = 0.7) {
